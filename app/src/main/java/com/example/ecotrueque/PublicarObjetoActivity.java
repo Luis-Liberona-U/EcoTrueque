b9647edbba41e2812dc0b1f86373f9ba295d5cc0
Luis <luisliberonadaniel@gmail.com>
2026-09-04 22:20:27 -0400

    feat: Implementación de botones y mensajes de error a la pantalla de PublicarObjetoActivity

--- app/src/main/java/com/example/ecotrueque/PublicarObjetoActivity.java
+++ app/src/main/java/com/example/ecotrueque/PublicarObjetoActivity.java
@@ -1,6 +1,12 @@
 package com.example.ecotrueque;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +21,56 @@ public class PublicarObjetoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_publicar_objeto);
+
+        EditText edtNombreObjeto = findViewById(R.id.edtNombreObjeto);
+
+        RadioGroup rgCategoria = findViewById(R.id.rgCategoria);
+
+        Button btnPublicar = findViewById(R.id.btnPublicar);
+
+        btnPublicar.setOnClickListener(v -> {
+
+            String nombreObjeto = edtNombreObjeto.getText().toString().trim();
+
+            if (nombreObjeto.isEmpty()){
+                edtNombreObjeto.setError("Ingrese el nombre del objeto");
+                edtNombreObjeto.requestFocus();
+                return;
+            }
+
+            int idCategoriaSeleccionada = rgCategoria.getCheckedRadioButtonId();
+
+            if (idCategoriaSeleccionada == -1){
+                Toast.makeText(PublicarObjetoActivity.this,
+                        "Seleccione una opcion",
+                        Toast.LENGTH_SHORT).show();
+
+                return;
+            }
+
+            RadioButton rbCategoriaSeleccionada = findViewById(idCategoriaSeleccionada);
+
+            String categoria = rbCategoriaSeleccionada.getText().toString();
+
+            Intent intent = new Intent(
+                    PublicarObjetoActivity.this,
+                    DetalleObjetoActivity.class
+            );
+
+            intent.putExtra("NOMBRE_OBJETO", nombreObjeto);
+            intent.putExtra("CATEGORIA_OBJETO", categoria);
+
+            startActivity(intent);
+
+
+
+
+
+
+
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
